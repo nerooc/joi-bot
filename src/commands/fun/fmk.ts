310effce414c45a367c792025f4176cf0f6e2af6
nerooc <tomasz_gajda@outlook.com>
2022-12-16 00:54:27 +0100

    Fix the issue with template string

--- src/commands/fun/fmk.ts
+++ src/commands/fun/fmk.ts
@@ -20,6 +20,6 @@ export const execute = async (interaction: CommandInteraction) => {
     users[i] = interaction.guild?.members.cache.random()?.user.username;
   }
 
-  const msg = `${author} + ": \nzabije " + ${users[0]} + ", \npoślubi " + ${users[1]} + " \ni cimcirimci ram pam pam z " + ${users[2]}`;
+  const msg = `${author}: \nzabije ${users[0]}, \npoślubi ${users[1]} \ni cimcirimci ram pam pam z ${users[2]}`;
   await interaction.reply(msg);
 };
