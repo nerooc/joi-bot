9368689533dfcfa0dbcf648df5070e9ba500836f
nerooc <tomasz_gajda@outlook.com>
2022-12-16 12:38:18 +0100

    Add a test command to check if build can succeed

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,4 +2,4 @@ export * from "./music";
 export * from "./games";
 export * from "./openai";
 export * from "./fun";
-// export * from "./utility";
+export * from "./utility";
---
+++ src/commands/utility/index.ts
@@ -0,0 +1,1 @@
+export * as testCommand from "./test-command";
