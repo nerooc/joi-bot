af6755994d0bc3e094c1b216f50edfaadb450a5f
nerooc <tomasz_gajda@outlook.com>
2022-12-16 12:53:31 +0100

    Delete test-command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,4 +2,3 @@ export * from "./music";
 export * from "./games";
 export * from "./openai";
 export * from "./fun";
-export * from "./utility";
--- src/commands/utility/index.ts
+++
@@ -1,1 +0,0 @@
-export * as testCommand from "./testCommand";
