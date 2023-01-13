d0b03f573354bb470bd26c7a6ac847825d52459a
nerooc <tomasz_gajda@outlook.com>
2023-01-13 09:55:35 +0100

    Add terraform for Firestore db creation, add gitignore terraform rules

--- terraform/gcp-joi.tfvars
+++
@@ -1,4 +0,0 @@
-project_id = “joi-discord-bot”
-region = “europe-central2”
-zone = “europe-central2-a”
-tf_service_account = “443899755090-compute@developer.gserviceaccount.com”
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,32 @@
-provider “google” {
-    project = var.project_id
-    region = var.region
-    zone = var.zone
-    impersonate_service_account = var.tf_service_account
+provider "google" {
+  project = "joi-terraform-test-3"
+  region = "europe-central2"
+  zone = "europe-central2-a"
 }
 
-resource "google_project" "my_project" {
-  name = "Firestore"
-  project_id = "terraform-test-joi"
+// Create GCP project
+resource "google_project" "joi-terraform-test-3" {
+  name = "JOI Terraform test"
+  project_id = "joi-terraform-test-3"
 }
 
-resource "google_app_engine_application" "app" {
-  project     = google_project.my_project.project_id
-  location_id = "europe-central2-a"
+// Enable Firestore API
+resource google_project_service "firestore" {
+  project = "${google_project.joi-terraform-test-3.project_id}"
+  service = "firestore.googleapis.com"
+}
+
+// Create Firestore database
+resource "google_app_engine_application" "database-app" {
+  project     = "${google_project.joi-terraform-test-3.project_id}"
+  location_id = "europe-central2"
   database_type = "CLOUD_FIRESTORE"
+}
+
+// Add a test document to the Firestore database
+resource "google_firestore_document" "test-document" {
+    project     = "${google_project.joi-terraform-test-3.project_id}"
+    collection  = "user_requests"
+    document_id = "nerooc-2023-01-13T09:30:35Z"   
+    fields       = "{\"request\":{\"mapValue\":{\"fields\":{\"command\":{\"stringValue\":\"play\"}}}}}"
 }
